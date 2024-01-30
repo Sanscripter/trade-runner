da64f3b753676e872e6682092101b3cff268532e
Guillermo Murúa <icguillermo.murua@americanwell.com>
2024-01-29 22:48:09 -0300

    Partially rewrote location screen to test new designs and begin implementing trading logic

--- src/app/map/location/location.component.ts
+++ src/app/map/location/location.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { GameService } from '../../shared/game.service';
 import ICity from '../../utils/ICity.interface';
+import { Item } from '../../game/Item';
 
 @Component({
   selector: 'app-location',
@@ -12,23 +13,59 @@ export class LocationComponent implements OnInit {
 
   locationId!: number;
   location!: ICity;
+  tradeBasket: Record<string,Array<Item>> = {playerSells: [], locationSells: []};
 
   constructor(private router: Router, private route: ActivatedRoute, public gameService: GameService) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe(qp => {
       this.locationId = qp['id'];
-      console.log(this.gameService);
-      console.log(this.locationId);
       this.location = this.gameService.cities.find(c => c.id == this.locationId)!;
-      console.log(this.location);
     });
-
   }
 
   handleGoBack() {
     this.gameService.advanceDay();
     this.router.navigate(['map']);
   }
+
+ handleAddedPlayer(item: Item){
+  //if the item is already in the array, don't increase the quantity
+  const existingItem = this.tradeBasket['playerSells'].find(i => i.name == item.name);
+  if (existingItem) {
+    existingItem.quantity!++;
+  }
+  else {
+    this.tradeBasket['playerSells'].push(item);
+  }
+ }
+
+ handleRemovedPlayer(item:Item){
+  const existingItem = this.tradeBasket['playerSells'].find(i => i.name == item.name);
+  if (existingItem) {
+    existingItem.quantity!--;
+  }
+  else {
+    this.tradeBasket['playerSells'].push(item);
+  }
+ }
+ handleAddedLocation(item: Item){
+  const existingItem = this.tradeBasket['locationSells'].find(i => i.name == item.name);
+  if (existingItem) {
+    existingItem.quantity!++;
+  }
+  else {
+    this.tradeBasket['locationSells'].push(item);
+  }
+ }
+ handleRemovedLocation(item: Item){
+  const existingItem = this.tradeBasket['locationSells'].find(i => i.name == item.name);
+  if (existingItem) {
+    existingItem.quantity!--;
+  }
+  else {
+    this.tradeBasket['locationSells'].push(item);
+  }
+ }
 
 }
