824995d9cae04439e3f7a192e9e9699b397c57e4
Guillermo Murúa <icguillermo.murua@americanwell.com>
2024-01-29 22:45:55 -0300

    Updated stats components to more generally accept location and player data

--- src/app/shared/stats/stats.component.ts
+++ src/app/shared/stats/stats.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { Player } from '../../game/Player';
+import ICity from '../../utils/ICity.interface';
 
 @Component({
-  selector: 'app-player-stats',
+  selector: 'app-agent-stats',
   templateUrl: './stats.component.html',
   styleUrl: './stats.component.scss'
 })
 export class StatsComponent {
 
-  @Input() player!: Player;
+  @Input() stats!: Player | ICity;
   @Input() day: number = 0;
+
+  get isPlayer() {
+    return this.stats instanceof Player;
+  }
 
 }
