ece218da3c00f58e76aa0459199f44d8f955bd26
Daniel Cazé <danielctlima@gmail.com>
2024-09-25 14:21:30 -0300

    electron build

---
+++ main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  if (process.env.NODE_ENV === "development") {
+    win.loadURL("http://localhost:4200");
+  } else {
+    win.loadFile(
+      path.join(__dirname, "dist/trading-grounds/browser/index.html")
+    );
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
